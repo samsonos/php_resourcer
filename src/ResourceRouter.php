8bdaa636b21001500d3437c00733d6bc0225c34d
Pavlo Onysko <onysko@samsonos.com>
2015-12-25 17:49:26 +0200

    Update ResourceRouter.php

--- src/ResourceRouter.php
+++ src/ResourceRouter.php
@@ -212,7 +212,7 @@ class ResourceRouter extends ExternalModule
         // Если мы нашли шаблон - переберем все найденные патерны
         if (isset($matches[2]) && strpos($matches[2], 'data:') === false) {
             // Remove relative path from resource path
-            $url = str_replace('../','', $matches[2]);
+            $url = str_replace('../', '/', $matches[2]);
 
             // Routes with this module controller do not need changes
             if(strpos($url, '/'.$this->id.'/') === false) {
